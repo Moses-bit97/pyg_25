8fd0fd6a12ee27e45da42746479ac1f9b8c5bc08
Moses Fetuah <fetuahmoses@gmail.com>
2020-07-31 19:57:20 +0000

    Display time tracker summary

--- time_tracker.py
+++ time_tracker.py
@@ -32,3 +32,25 @@ print(f"You completed your task at: {end}")
 
 duration = end_datetime - start_datetime
 earning = round((duration.total_seconds()/3600)*5, 2)
+
+def format_duration(duration):
+    total_seconds = duration.seconds
+    hours = divmod(total_seconds, 3600)[0]
+    minutes = divmod(total_seconds, 60)
+    return f"{hours}Hrs:{minutes[0]}mins:{minutes[1]}sec"
+
+ 
+
+
+formatted_duration = format_duration(duration)
+
+ 
+
+print(
+    f'''
+    | Task | Start Date | Start Time | Completion Date | Completion Time | Duration (Hrs) | Earnings($) |
+    -----------------------------------------------------------------------------------------------------
+    | {task_name} | {start_date}| {start} | {end_date}  | {end} | | {formatted_duration} | ${earning} |
+    '''
+)
+
